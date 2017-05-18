cd26224423895c851d032c4b45f7dba410e40ac0
Alexa Kur <alexa.kur@gmail.com>
2017-05-18 15:29:02 +0200

    -

--- software/step1.5_filter_and_transform.R
+++ software/step1.5_filter_and_transform.R
@@ -3,7 +3,7 @@ input = options[1]
 output = options[2]
 cutoff = as.numeric(options[3])
 data = read.table(input, header=T,row.names=1,sep="\t")
-data = data[,(colSums(data == 0) =< cutoff*nrow(data))]
+data = data[,(colSums(data > 0) >= cutoff*nrow(data))]
 data = data[, grep("NOTAX",colnames(data),invert = T)]
 data = data[,grep("domain",colnames(data),invert = T)]
 data = data[,grep("rootrank.Root",colnames(data),invert = T)]
