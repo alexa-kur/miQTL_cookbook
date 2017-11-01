370e9d2c7723fe1aa89d3f4ee70d3ae58e11c457
Alexa Kur <alexa.kur@gmail.com>
2017-11-01 15:28:45 +0100

    small fixes

--- software/step3.5_pcoa.R
+++ software/step3.5_pcoa.R
@@ -1,5 +1,8 @@
 options = commandArgs(trailingOnly = TRUE)
 input = options[1]
+phenos_input = options[2]
+coupling_file = options[3]
+
 data = read.delim(input, header=T,row.names=1,sep="\t")
 data = data[,(colSums(data > 0) >= 0.1)]
 data = data[, grep("genus.",colnames(data))]
@@ -8,3 +11,29 @@ pco1 = capscale(data ~ 0,distance = "bray")
 pcs = pco1$CA$u[,1:10]
 rownames(pcs) = paste0("Sample",1:nrow(pcs))
 write.table(pcs, file = "10PCS.txt",sep="\t")
+phenos = read.delim(phenos_input, )
+
+if (exists("coupling_file")&file.exists(coupling_file)){
+  coupling = read.table(coupling_file,as.is = T)
+  has_both = (coupling[,1] %in% rownames(phenos)) & (coupling[,2] %in% rownames(data))
+  coupling= coupling[has_both,]
+  tax = data[coupling[,2],]
+  phenos = phenos[coupling[,1],,drop = FALSE]
+  rownames(tax) = rownames(phenos)
+  tax = tax[rownames(tax)[grep("[.][0-9]+$",rownames(tax),invert=T)],]
+  phenos = phenos[rownames(tax),,drop = FALSE]
+} else {
+  tax = data[intersect(rownames(data),rownames(phenos)),]
+  phenos = phenos[rownames(tax),,drop = FALSE]
+}
+
+dist.mat = vegdist(tax,method = "bray")
+results = list()
+for (i in 1:ncol(phenos)){
+  ad1 = adonis(dist.mat ~ phenos[,i],permutations = 1000)
+  results[[i]] = ad1$aov.tab[1,]
+}
+
+ad_results = do.call(rbind,results)
+rownames(ad_results) = colnames(phenos)
+write.table(ad_results,file = "adonis_covariates.txt",sep="\t")
